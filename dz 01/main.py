5debf55dccf37aae9b1864e6d859abc44a22cd91
Peps@ <352481@mail.ru>
2024-02-17 16:54:44 +0300

    Добавил тесты

--- dz 01/main.py
+++ dz 01/main.py
@@ -9,3 +9,10 @@ def find_occurence(list1: list, list2: list) -> dict:
         if i in list2:
             occurrence[i] = occurrence.get(i, 0) + 1
     return occurrence
+
+def main():
+    list1 = [1, 2, 3, 3, 3, 4, 4, 7, 8, 9, 1]
+    list2 = [1, 3, 4, 8, 9]
+    print(find_occurence(list1, list2))
+if __name__ == "__main__":
+    main()
